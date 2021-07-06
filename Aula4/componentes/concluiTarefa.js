0993311d9e5f2c5b91cc370232b4f23d2bd2c427
Bruno-Costa <brunocosta.fig@gmail.com>
2021-07-06 06:25:06 -0300

    projeto finalizado

---
+++ Aula4/componentes/concluiTarefa.js
@@ -0,0 +1,23 @@
+const concluirTarefa = (atualiza, id) => {
+    const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas'))
+
+    tarefasCadastradas[id].concluida = !tarefasCadastradas[id].concluida
+    localStorage.setItem('tarefas', JSON.stringify(tarefasCadastradas))
+
+    atualiza()
+}
+
+const BotaoConclui = (atualiza, id) => { 
+    const botaoConclui = document.createElement('button')  
+    
+    botaoConclui.classList.add('check-button')
+    botaoConclui.innerText = 'concluir'
+
+    botaoConclui.addEventListener('click', () => concluirTarefa(atualiza, id))
+
+    return botaoConclui
+
+}
+
+export default BotaoConclui
+
---
+++ Aula4/componentes/criaTarefa.js
@@ -0,0 +1,52 @@
+import { carregaTarefa } from './carregaTarefa.js'
+import BotaoConclui from './concluiTarefa.js'
+import BotaoDeleta from './deletaTarefa.js'
+ 
+
+export const handleNovoItem = (evento) => {
+    evento.preventDefault()
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'))||[]
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+
+    const calendario = document.querySelector('[data-form-date]')
+    const data = moment(calendario.value)
+    const horario = data.format("HH:mm")
+
+    const dataFormatada = data.format('DD/MM/YYYY')
+    const concluida = false
+
+    const dados = { 
+        valor,
+        dataFormatada,
+        horario,
+        concluida
+    }
+
+    const tarefasAtualizadas = [...tarefas, dados]
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefasAtualizadas))
+
+    input.value = " "
+
+    carregaTarefa()
+    
+}
+
+export const Tarefa = ({ valor, horario, concluida }, id) => {
+
+    const tarefa = document.createElement('li')
+    const conteudo = `<p class="content">${horario} * ${valor}</p>`
+    if(concluida){
+        tarefa.classList.add('done')
+    }
+
+    tarefa.classList.add('task')
+    tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(BotaoConclui(carregaTarefa, id))
+    tarefa.appendChild(BotaoDeleta(carregaTarefa, id))
+   
+    return tarefa
+
+}
